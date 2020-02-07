4db3f422bfb0487409e29230ee51b2767e18d0a4
Akash <ap983983@gmail.com>
2020-02-08 00:49:22 +0530

    v.0.0

---
+++ 07-interfaces/CricketCoach.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CricketCoach = /** @class */ (function () {
+    function CricketCoach() {
+    }
+    CricketCoach.prototype.getDailyWorkout = function () {
+        return "Cricket Coach";
+    };
+    return CricketCoach;
+}());
+exports.CricketCoach = CricketCoach;
---
+++ 07-interfaces/GolfCoach.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var GolfCoach = /** @class */ (function () {
+    function GolfCoach() {
+    }
+    GolfCoach.prototype.getDailyWorkout = function () {
+        return "Golf Coach";
+    };
+    return GolfCoach;
+}());
+exports.GolfCoach = GolfCoach;
